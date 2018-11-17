2ee2bb1bc4aeb05894f614b8414e3665264610ed
Mário Santos <super.mario.santos.sousa@gmail.com>
2018-11-17 16:08:29 -0300

    hello world

---
+++ src/main/java/br/com/itsmemario/capjava/HelloWorld.java
@@ -0,0 +1,16 @@
+package br.com.itsmemario.capjava;
+
+/**
+ * Programa 1.1 – “HelloWorld.java”
+ * 
+ * @author mario
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
